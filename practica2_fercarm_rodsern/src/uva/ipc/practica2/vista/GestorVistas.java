b2f276862a3205373d038bba828e4a87ef876626
fercarm <fernando.carmona22@estudiantes.uva.es>
2024-04-10 11:47:14 +0200

    Gestor de vistas completado

--- practica2_fercarm_rodsern/src/uva/ipc/practica2/vista/GestorVistas.java
+++ practica2_fercarm_rodsern/src/uva/ipc/practica2/vista/GestorVistas.java
@@ -1,13 +1,57 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package uva.ipc.practica2.vista;
 
+import javax.swing.JFrame;
+
 /**
- *
- * @author Fernando
+ * @author Fernando Carmona Palacio
+ * @author Rodrigo Serna Albarrán
  */
 public class GestorVistas {
+
+    private JFrame vistaActual;
+
+    public void mostrarVistaComprarBillete() {
+        if (vistaActual != null) {
+            vistaActual.setVisible(false);
+            vistaActual.dispose();
+        }
+        vistaActual = new VistaComprarBillete();
+        vistaActual.setVisible(true);
+    }
     
+    public void mostrarVistaMenuPrincipal() {
+        if (vistaActual != null) {
+            vistaActual.setVisible(false);
+            vistaActual.dispose();
+        }
+        vistaActual = new VistaMenuPrincipal();
+        vistaActual.setVisible(true);
+    }
+    
+    public void mostrarVistaMenuUsuario() {
+        if (vistaActual != null) {
+            vistaActual.setVisible(false);
+            vistaActual.dispose();
+        }
+        vistaActual = new VistaMenuUsuario();
+        vistaActual.setVisible(true);
+    }
+    
+    public void mostrarVistaMisViajes() {
+        if (vistaActual != null) {
+            vistaActual.setVisible(false);
+            vistaActual.dispose();
+        }
+        vistaActual = new VistaMisViajes();
+        vistaActual.setVisible(true);
+    }
+    
+    public void mostrarVistaRecargarTarjeta() {
+        if (vistaActual != null) {
+            vistaActual.setVisible(false);
+            vistaActual.dispose();
+        }
+        vistaActual = new VistaRecargarTarjeta();
+        vistaActual.setVisible(true);
+    }
 }
